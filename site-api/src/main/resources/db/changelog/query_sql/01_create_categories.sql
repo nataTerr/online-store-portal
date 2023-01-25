4fd743911c0fad6d834b099a7d9317c752101d18
NataTerr <karachevan@list.ru>
2023-01-25 22:02:15 +0300

    add files for site_api in resources (sql script, liquibase, csv)

--- site-api/src/main/resources/db/changelog/query_sql/01_create_categories.sql
+++ site-api/src/main/resources/db/changelog/query_sql/01_create_categories.sql
@@ -1,5 +1,5 @@
 --liquibase formatted sql
---changeset demo:data-01
+--changeset demo:query-01
 CREATE TABLE IF NOT EXISTS site_api.categories (
     id integer PRIMARY KEY unique not null,
     caption VARCHAR(255) unique not null,
