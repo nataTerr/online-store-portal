11be32506d0000947d2a2c53fc16afb8dc8f7e21
NataTerr <karachevan@list.ru>
2023-04-14 23:00:22 +0300

    feature logger + в pom исправлен parent на spring-boot-starter-parent

--- site-api/src/main/java/nata/project/service/CategoryServiceImpl.java
+++ site-api/src/main/java/nata/project/service/CategoryServiceImpl.java
@@ -6,6 +6,8 @@ import nata.project.dtos.converters.CategoryToDtoConverter;
 import nata.project.dtos.response.BreadcrumbsCategoriesDto;
 import nata.project.dtos.response.CategoryDto;
 import nata.project.repository.CategoryRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,10 +20,12 @@ public class CategoryServiceImpl implements CategoryService {
     private final CategoryRepository categoryRepository;
     private final CategoryToDtoConverter categoryConverter;
     private final BreadcrumbsCategoriesToDtoConverter breadcrumbsConverter;
+    private static final Logger logger = LoggerFactory.getLogger(CategoryServiceImpl.class);
 
     @Override
     @Transactional(readOnly = true)
     public List<CategoryDto> getCatalog() {
+        logger.info("Show store catalog (method call findAllByParentIdIsNull in CategoryRepository)");
         return categoryRepository.findAllByParentIdIsNull().stream()
                 .map(categoryConverter::convert)
                 .collect(Collectors.toList());
@@ -29,6 +33,7 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public List<BreadcrumbsCategoriesDto> breadcrumbs(Integer categoryId) {
+        logger.info("Receive breadcrumbs by category id {}", categoryId);
         return categoryRepository.breadcrumbs(categoryId).stream()
                 .map(breadcrumbsConverter::convert)
                 .collect(Collectors.toList());
@@ -38,6 +43,8 @@ public class CategoryServiceImpl implements CategoryService {
     @Transactional(readOnly = true)
     // todo: надо переделать, т.к.запрос несет много лишней информации
     public List<CategoryDto> findAllByParentId(Integer parentId) {
+        logger.info("Get a list of categories by parent {} as a hierarchy " +
+                "(method call findAllByParentId in CategoryRepository)", parentId);
         return categoryRepository.findAllByParentId(parentId).stream()
                 .map(categoryConverter::convert)
                 .collect(Collectors.toList());
@@ -45,6 +52,8 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public List<Integer> getFlatCategoryTree(Integer parentId) {
+        logger.info("Find all child categories by parent {} " +
+                "(method call findAllChildCategoriesByParent in CategoryRepository)", parentId);
         return categoryRepository.findAllChildCategoriesByParent(parentId);
     }
 }
