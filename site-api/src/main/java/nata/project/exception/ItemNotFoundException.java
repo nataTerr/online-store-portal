749cdf9c3826dc784f8ae36df91abe35ef6602be
NataTerr <karachevan@list.ru>
2023-05-05 13:29:22 +0300

    review (GlobalExceptionHandler)

--- site-api/src/main/java/nata/project/exception/ItemNotFoundException.java
+++ site-api/src/main/java/nata/project/exception/ItemNotFoundException.java
@@ -1,12 +1,7 @@
 package nata.project.exception;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class ItemNotFoundException extends RuntimeException {
-    private static final Logger logger = LoggerFactory.getLogger(ItemNotFoundException.class);
     public ItemNotFoundException(String message) {
         super(message);
-        logger.error(message);
     }
 }
