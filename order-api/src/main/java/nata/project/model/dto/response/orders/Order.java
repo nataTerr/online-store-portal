85a1ceecbab5044bedbcd4da4b46c3cdd0f7949a
NataTerr <karachevan@list.ru>
2023-08-15 21:08:35 +0300

    review

--- order-api/src/main/java/nata/project/model/dto/response/orders/Order.java
+++ order-api/src/main/java/nata/project/model/dto/response/orders/Order.java
@@ -1,24 +1,18 @@
 package nata.project.model.dto.response.orders;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Order {
 
     private Long id;
     private Long productId;
     private int count;
     private Long userId;
-
-    public Order() {
-    }
-
-    public Order(Long id, Long productId, int count, Long userId) {
-        this.id = id;
-        this.productId = productId;
-        this.count = count;
-        this.userId = userId;
-    }
 }
